a9a4fb98542410a43459f513289656b452085006
Ian P. Christian <ian.christian@bjss.com>
2020-02-22 21:08:04 +0000

    Adding in a user

--- site.pp
+++ site.pp
@@ -1,19 +1,12 @@
 class { 'os_hardening': }
 class { 'ssh_hardening': }
 
-
-accounts::user { 'ec2-user':
-  ensure => 'absent',
-}
-
 accounts::user { 'pookey':
   comment => 'Ian P. Christian',
   groups  => [
     'admin',
     'wheel',
   ],
-  uid     => '1001',
-  gid     => '1001',
   sshkeys => [
     'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDK2I2PpV5hcnGVD9idGkzUg5kyPBu0oIYxJg0ZZ26krRxBlzBFsbQLBkh1MVwhEwnXh0XbXAEPOGUjTLllMUZ+bnSWwyl5QwODu2Z+f+AxJZOpUn8YRY9Lqmf7/DaosefuSgj+Wg2BHJS9xWdlC/zd9uC11CLNuX3FolPzWIEO3CHE3Q/m8kJm8FADvsxUFPZKoyGwlnnxED4RHSvFulY7lIOXCxmjqbh9OCJ6I0zkV0ED0psB8ilc8rR40hK8d299Rel2bTECGmPrYPbxSFwJg3IXFvNZstxtEWcFCUfSrYxS5Ch16EDNHhLUSh0K2/ir8OQxkX5Ay1/AxrRX5/LT pookey@pookey.co.uk',
   ],
